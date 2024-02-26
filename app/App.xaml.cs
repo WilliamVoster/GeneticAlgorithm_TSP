80dab16e9ba5a9c5fac4894f5c299cc7cc628e72
WilliamVoster <46726271+WilliamVoster@users.noreply.github.com>
2024-02-26 13:32:24 +0100

    Created applicaiton entry point

--- app/App.xaml.cs
+++ app/App.xaml.cs
@@ -5,10 +5,26 @@ using System.Windows;
 namespace app
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// App starting point
     /// </summary>
     public partial class App : Application
     {
+        [STAThread] // Single threaded application
+        public static void Main()
+        {
+            var application = new App();
+            var window = new MainWindow();
+
+            //window.Show();
+
+            application.Run(window);
+
+        }
+
+
     }
+
+
+
 
 }
