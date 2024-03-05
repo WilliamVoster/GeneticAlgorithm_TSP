67f9c81327efb13fb4eb23b64e4e10af7c177a25
WilliamVoster <46726271+WilliamVoster@users.noreply.github.com>
2024-03-05 13:36:01 +0100

    Created skeleton and class for reading JSON tsp file

---
+++ Program/Population.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Population
+    {
+        private Dictionary<Chromosome, Double> population;
+        public Population() 
+        {
+            this.population = new Dictionary<Chromosome, Double>();
+        }
+
+        
+    }
+}
---
+++ Program/TSPProblem.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class TSPProblem
+    {
+        public int nbr_nurses {  get; set; }
+        public string instance_name { get; set; }
+        public int capacity_nurse { get; set; }
+        public Depot depot { get; set; }
+        public Dictionary<int, Patient> patients { get; set; }
+        public Double[,] travel_times { get; set; }
+
+
+        public static TSPProblem readJSON(string filepath)
+        {
+            string jsonString = File.ReadAllText(filepath);
+            TSPProblem myProblem = JsonConvert.DeserializeObject<TSPProblem>(jsonString);
+            return myProblem;
+        }
+    }
+
+    internal class Depot
+    {
+        public int return_time { get; set; }
+        public int x_coord { get; set; }
+        public int y_coord { get; set; }
+    }
+    internal class Patient
+    {
+        public int x_coord { get; set; }
+        public int y_coord { get; set; }
+        public int demand { get; set; }
+        public int start_time { get; set; }
+        public int end_time { get; set; }
+        public int care_time { get; set; }
+    }
+}
